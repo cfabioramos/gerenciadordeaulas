280bbd3cedf827c2dfe5353e00f2f2933e798d91
Carlos Fabio <cfabioramos@gmail.com>
2026-08-03 08:56:23 -0300

    Springboot project initial files.

---
+++ src/main/java/com/br/gerenciadoraulas/GerenciadoraulasApplication.java
@@ -0,0 +1,13 @@
+package com.br.gerenciadoraulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadoraulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadoraulasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/gerenciadoraulas/GerenciadoraulasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.gerenciadoraulas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadoraulasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
