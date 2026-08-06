1a76cd9b8d7fc8f7829f65882a9aa4b3ca471f11
Carlos Fábio Ramos Conceição <cfabioramos@gmail.com>
2026-08-06 15:20:52 -0300

    Presenca services

--- src/main/java/com/br/gerenciadoraulas/repository/AlunoRepository.java
+++ src/main/java/com/br/gerenciadoraulas/repository/AlunoRepository.java
@@ -26,8 +26,8 @@ public interface AlunoRepository extends JpaRepository<Aluno, Long> {
                        CASE WHEN p.id IS NOT NULL THEN true ELSE false END AS presente
                 FROM aluno a
                 INNER JOIN matricula m ON m.aluno_id = a.id
-                INNER JOIN programa_aula pa ON pa.id = m.programa_aula_id
-                INNER JOIN aula au ON au.programa_aula_id = pa.id
+                INNER JOIN programa_aula pa ON pa.id = m.programaaula_id
+                INNER JOIN aula au ON au.programaaula_id = pa.id
                 LEFT JOIN presenca p ON p.matricula_id = m.id AND p.aula_id = :aulaId
                 WHERE au.id = :aulaId
             """, nativeQuery = true)
